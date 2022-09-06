0f2d1e9b1de0c79bd89184ca08284537ded5ce0a
Aurelien Gateau <aurelien.gateau@gitguardian.com>
2022-09-06 17:04:09 +0200

    wip: ScanProgressBar

--- ggshield/cmd/secret/scan/path.py
+++ ggshield/cmd/secret/scan/path.py
@@ -6,7 +6,29 @@ import click
 from ggshield.core.file_utils import get_files_from_paths
 from ggshield.core.utils import ScanContext, ScanMode, handle_exception
 from ggshield.output import OutputHandler
-from ggshield.scan import File, ScanCollection
+from ggshield.scan import File, ScanCollection, Files
+
+
+class ScanProgressBar:
+    """
+    Creates a click progress bar to monitor a scan. Usage:
+
+    ```
+    with ScanProgressBar(files) as progressbar:
+    ```
+    """
+    def __init__(self, files: Files):
+        self._bar = click.progressbar(length=len(files.files), label="Scanning", file=sys.stderr)
+
+    def on_file_chunk_scanned(self, chunk: List[File]) -> None:
+        self._bar.update(len(chunk))
+
+    def __enter__(self) -> "ScanProgressBar":
+        self._bar.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self._bar.__exit__(exc_type, exc_val, exc_tb)
 
 
 @click.command()
@@ -34,27 +56,21 @@ def path_cmd(
             # when scanning a path explicitly we should not care if it is a git repository or not
             ignore_git=True,
         )
-        with click.progressbar(
-            length=len(files.files), label="Scanning", file=sys.stderr
-        ) as progressbar:
+        scan_context = ScanContext(
+            scan_mode=ScanMode.PATH,
+            command_path=ctx.command_path,
+        )
 
-            def update_progress(chunk: List[File]) -> None:
-                progressbar.update(len(chunk))
-
-            scan_context = ScanContext(
-                scan_mode=ScanMode.PATH,
-                command_path=ctx.command_path,
-            )
-
+        with ScanProgressBar(files) as progressbar:
             results = files.scan(
                 client=ctx.obj["client"],
                 cache=ctx.obj["cache"],
                 matches_ignore=config.secret.ignored_matches,
                 scan_context=scan_context,
                 ignored_detectors=config.secret.ignored_detectors,
-                on_file_chunk_scanned=update_progress,
+                on_file_chunk_scanned=progressbar.on_file_chunk_scanned,
             )
-            scan = ScanCollection(id=" ".join(paths), type="path_scan", results=results)
+        scan = ScanCollection(id=" ".join(paths), type="path_scan", results=results)
 
         return output_handler.process_scan(scan)
     except Exception as error:
